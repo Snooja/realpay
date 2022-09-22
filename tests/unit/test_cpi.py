cc7c2be4c9e07d4eefab4e1d5910c57f6eec412f
Jade Forlani-Brennan <jade@192-168-1-106.tpgi.com.au>
2022-09-22 16:56:20 +0800

    :white_check_mark: from_csv working

--- tests/unit/test_cpi.py
+++ tests/unit/test_cpi.py
@@ -2,6 +2,8 @@
 # Public Packages
 import pytest
 import pandas as pd
+from pathlib import Path
+
 
 # Internal Imports
 from realpay.cpi import CPI
@@ -16,13 +18,14 @@ def cpi_df():
 
 @pytest.fixture(scope="session")
 def cpi_csv(tmp_path_factory, cpi_df):
-    fp = tmp_path_factory.mktemp('data','test_cpi.csv')
+    fp = tmp_path_factory.mktemp('data') / 'test_cpi.csv'
     cpi_df.to_csv(fp, header=True,index=False)
     return fp
 
 class TestCPI:
     def test_from_csv(self, cpi_csv):
-        CPI.from_csv(cpi_csv)
+        test = CPI.from_csv(cpi_csv)
+        print(test.raw)
         
 
         
